ab5fa0029b321f6db1562db114c38d4a458d7fcf
tgilvonas <tgilvonas@gmail.com>
2025-10-22 20:33:03 +0300

    Website logo was fixed

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -4,7 +4,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 
 <template>
     <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
+        class="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground"
     >
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
