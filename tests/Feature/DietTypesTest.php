54df6c4c249f9e9cb8cd73eae282b0cdd16a17e8
tgilvonas <tgilvonas@gmail.com>
2026-03-14 21:13:09 +0200

    Diet types test added

---
+++ tests/Feature/DietTypesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Database\Seeders\DietTypesSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DietTypesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_unauthenticated_user_can_not_see_diet_types()
+    {
+        $response = $this->get(route('diet_types.index'));
+        $response->assertRedirect(route('login'));
+
+        $response = $this->get(route('diet_types.json_list'));
+        $response->assertRedirect(route('login'));
+    }
+
+    public function test_authenticated_user_can_see_ingredients()
+    {
+        $user = User::factory()->create();
+
+        $this->seed(DietTypesSeeder::class);
+
+        $response = $this->actingAs($user)->get(route('diet_types.index'));
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($user)->get(route('diet_types.json_list'));
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($user)->get(route('diet_types.json_list', [
+            'order_by_field' => 'title',
+            'order_by_direction' => 'asc',
+            'page' => 1,
+            'paginate_by' => 10,
+        ]));
+        $response->assertStatus(200);
+    }
+}
